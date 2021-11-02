38b5e208b0a0fbb1755cac7ed665eb68410ea989
Dmitry Suldin <d.suldin@ongrid.pro>
2021-11-02 12:43:21 +0300

    feat: upgrade battleverse

--- battleverse/src/App.js
+++ battleverse/src/App.js
@@ -1,26 +1,93 @@
 import { useState } from 'react'
 import { useWeb3React } from '@web3-react/core'
+import CircularProgress from '@mui/material/CircularProgress'
 
-import { Web3status } from "./components/Web3status/Web3status";
-import { Skill } from "./components/Web3status/Skill";
+import Logo from './assets/img/logo.png'
+import Bot from './assets/img/bot.jpg'
+import { sendYourselfZeroETH } from './api'
+import { Web3status } from './components/Web3status/Web3status'
 
 export function App() {
-  const { active } =
-    useWeb3React()
+  const { active } = useWeb3React()
   const [skill, setSkill] = useState(0)
+  const [pending, setPending] = useState(false)
 
   return (
     <div className='wrapper'>
-      <div className='token'><img src="https://api.onbridge.io/token_metadata/0.jpeg"/></div>
-      <div className='tokenname'>Baby battle bot #0</div>
-      <div className='logo'><img src="https://uploads-ssl.webflow.com/615802989bf0553e8b33da93/615ca4091ba50ae18c5fbe0a_lo.png"/></div>
-      <div className='text'><Web3status /></div>
-      {active && <div className="counter">
-        <Skill skill={ skill }/>
-        <button className="button incrementbutton" onClick={() => setSkill(skill + 100)}>Play and Earn</button>
+      <div className='logo'>
+        <img src={Logo} alt='Logo' />
       </div>
-      }
-      
+
+      <div className='content'>
+        <img className='token' src={Bot} alt='Bot' />
+
+        <div className='tokenname text'>Baby battle bot # 0</div>
+
+        <div className='text'>
+          <Web3status />
+        </div>
+
+        {active && !pending && (
+          <div className='counter'>
+            <div className='text'>Skill: {skill}</div>
+
+            <div className='buttons-wrapper'>
+              <button
+                className='button incrementbutton'
+                onClick={() => {
+                  sendYourselfZeroETH()
+                    .then(tx => {
+                      setPending(true)
+
+                      tx.wait()
+                        .then(() => {
+                          setPending(false)
+                          setSkill(skill + 100)
+                        })
+                        .catch(() => {
+                          setPending(false)
+                        })
+                    })
+                    .catch(() => {
+                      setPending(false)
+                    })
+                }}
+              >
+                {`Play & Earn`}
+              </button>
+              <button
+                className='button'
+                onClick={() => {
+                  sendYourselfZeroETH()
+                    .then(tx => {
+                      setPending(true)
+
+                      tx.wait()
+                        .then(() => {
+                          setPending(false)
+                        })
+                        .catch(() => {
+                          setPending(false)
+                        })
+                    })
+                    .catch(() => {
+                      setPending(false)
+                    })
+                }}
+              >
+                Withdraw
+              </button>
+            </div>
+          </div>
+        )}
+
+        {pending && (
+          <div className='loader'>
+            <CircularProgress />
+            <div className='loader-message'>Tx. is pending</div>
+          </div>
+        )}
+      </div>
     </div>
-  );
+  )
 }
