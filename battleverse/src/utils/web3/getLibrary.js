e637d4184d4e6fc5a339533c30b91a91bddff918
Alex Skipper <skipper@onbridge.io>
2022-02-10 02:47:47 +0300

    feat: v0.2.0-debridge-kovan-bsc

--- battleverse/src/utils/web3/getLibrary.js
+++
@@ -1,7 +0,0 @@
-import { Web3Provider } from '@ethersproject/providers'
-
-export function getLibrary(provider) {
-  const library = new Web3Provider(provider)
-  library.pollingInterval = 8000
-  return library
-}
